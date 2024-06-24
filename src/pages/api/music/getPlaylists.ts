7c8067c60580994fb3e178798edd26d06b457311
Adrià Cabrera <adriacabrera2007@gmail.com>
2024-06-24 22:25:20 +0200

    feat: Add @headlessui/react dependency for improved UI components

---
+++ src/pages/api/music/getPlaylists.ts
@@ -0,0 +1,40 @@
+import { supabase } from '@/lib/supabase';
+
+export async function GET({
+    params,
+    request,
+    redirect,
+}: {
+    params: any;
+    request: any;
+    redirect: any;
+}) {
+    // get the id from the url search params
+
+    //console.log(album_id);
+    const {
+        data: { user },
+    } = await supabase.auth.getUser();
+
+    const userdata = await user;
+    //console.log(userdata);
+    try {
+        //console.log(userdata?.id);
+
+        let { data: playlists, error } = await supabase
+            .from('playlists')
+            .select('playlist_id, name, description, cover')
+            .eq('user_id', userdata?.id);
+
+        if (error) {
+            return new Response(error.message, { status: 500 });
+        }
+
+        return new Response(JSON.stringify(playlists), {
+            headers: { 'content-type': 'application/json' },
+        });
+    } catch (error: any) {
+        console.log(error.message);
+        return redirect('/signin');
+    }
+}
