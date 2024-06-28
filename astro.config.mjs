00468b215e103c30878a1f40356be92e9d2333d0
Adrià Cabrera <adriacabrera2007@gmail.com>
2024-06-28 20:17:50 +0200

    refactor: Update astro.config.mjs to use astro-compressor and astro-min for minification

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import vercel from '@astrojs/vercel/serverless';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 import svelte from '@astrojs/svelte';
+import minify from 'astro-min';
+import compressor from 'astro-compressor';
 
 // https://astro.build/config
 export default defineConfig({
@@ -13,5 +15,26 @@ export default defineConfig({
             enabled: true,
         },
     }),
-    integrations: [tailwind(), react(), svelte()],
+    integrations: [
+        tailwind(),
+        react(),
+        svelte(),
+        minify({
+            do_not_minify_doctype: false,
+            // ensure_spec_compliant_unquoted_attribute_values: false,
+            // keep_closing_tags: false,
+            // keep_comments: false,
+            // keep_html_and_head_opening_tags: false,
+            // keep_input_type_text_attr: false,
+            // keep_spaces_between_attributes: false,
+            // keep_ssi_comments: false,
+            minify_css: true,
+            minify_js: true,
+            // preserve_brace_template_syntax: false,
+            // preserve_chevron_percent_template_syntax: false,
+            // remove_bangs: false,
+            // remove_processing_instructions: false,
+        }),
+        compressor(),
+    ],
 });
