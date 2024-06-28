7979c3a228b98f4270dc83688c168c55cdba1119
Adrià Cabrera <adriacabrera2007@gmail.com>
2024-06-28 18:46:45 +0200

    refactor: Remove commented out code for minifying CSS and JS

--- astro.config.mjs
+++ astro.config.mjs
@@ -17,29 +17,7 @@ export default defineConfig({
             enabled: true,
         },
     }),
-    integrations: [
-        tailwind(),
-        react(),
-        svelte(),
-        minify({
-            //do_not_minify_doctype: false,
-            // ensure_spec_compliant_unquoted_attribute_values: false,
-            // keep_closing_tags: false,
-            // keep_comments: false,
-            // keep_html_and_head_opening_tags: false,
-            // keep_input_type_text_attr: false,
-            // keep_spaces_between_attributes: false,
-            // keep_ssi_comments: false,
-            //minify_css: true,
-            //minify_js: true,
-            // preserve_brace_template_syntax: false,
-            // preserve_chevron_percent_template_syntax: false,
-            // remove_bangs: false,
-            // remove_processing_instructions: false,
-        }),
-
-        compressor(),
-    ],
+    integrations: [tailwind(), react(), svelte()],
     ssr: {
         async renderToHtml(render) {
             // Use react-ssr-prepass to ensure async rendering
