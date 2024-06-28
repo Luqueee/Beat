d8c800122b764351a9905bf058b06130e5207be0
Adrià Cabrera <adriacabrera2007@gmail.com>
2024-06-28 21:07:01 +0200

    refactor: Update astro.config.mjs to use tailwind and vercel plugins

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
-import { defineConfig } from "astro/config";
-import vercel from "@astrojs/vercel/serverless";
-import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import svelte from "@astrojs/svelte";
-import minify from "astro-min";
-import compressor from "astro-compressor";
+import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
+
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
@@ -20,22 +19,6 @@ export default defineConfig({
     tailwind(),
     react(),
     svelte(),
-    minify({
-      do_not_minify_doctype: false,
-      // ensure_spec_compliant_unquoted_attribute_values: false,
-      // keep_closing_tags: false,
-      // keep_comments: false,
-      // keep_html_and_head_opening_tags: false,
-      // keep_input_type_text_attr: false,
-      // keep_spaces_between_attributes: false,
-      // keep_ssi_comments: false,
-      minify_css: true,
-      minify_js: true,
-      // preserve_brace_template_syntax: false,
-      // preserve_chevron_percent_template_syntax: false,
-      // remove_bangs: false,
-      // remove_processing_instructions: false,
-    }),
-    compressor(),
+    
   ],
 });
